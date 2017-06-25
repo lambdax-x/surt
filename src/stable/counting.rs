df26eeb1fc6b581b9d32d72d12a0c677a9b7c4d0
λx.x <lambdax-x@users.noreply.github.com>
2017-06-25 20:31:25 +0200

    Make counting sort more generic
    The implementation can sort any type T, the drawback is the need of a
    ranking function Fn(&T) -> usize

--- src/stable/counting.rs
+++ src/stable/counting.rs
@@ -1,27 +1,34 @@
-pub fn sort(max: u32, v: &mut [u32]) {
-    let count_len = (max + 1) as usize;
+pub fn sort<T: Copy, F>(k: usize, v: &mut [T], rank: F)
+where F: Fn(&T) -> usize {
+    let count_len = k + 1;
     let mut count: Vec<usize> = vec![0; count_len];
 
-    for e in v.iter() {
-        let i = *e as usize;
-        count[i] += 1;
+    {
+        let v_ranked = v.iter().map(|x| rank(x));
+        for i in v_ranked {
+            count[i] += 1;
+        }
     }
-    /// count[e]: number of occurences of e in v
+    /// count[i]: number of occurences of i in v.map(rank)
 
     for i in 1 .. count_len {
         count[i] += count[i - 1];
     }
-    /// count[e]: number of occurences less or equal than e in v
+    /// count[i]: number of occurences less or equal than i in v.map(rank)
 
-    let mut w = vec![0; v.len()];
-
-    for e in v.iter().rev() {
-        let i = *e as usize;
-        w[count[i] - 1] = *e;
-        count[i] -= 1;
+    let mut w: Vec<Option<T>> = vec![None; v.len()];
+    {
+        let v_ranked = v.iter().map(|x| rank(x));
+        for (e, i) in v.iter().zip(v_ranked).rev() {
+            w[count[i] - 1] = Some(e.clone());
+            count[i] -= 1;
+        }
     }
 
     for (to, from) in v.iter_mut().zip(w.iter()) {
-        *to = *from;
+        match *from {
+            Some(a) => *to = a,
+            _ => {}
+        }
     }
 }
