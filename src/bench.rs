8298609fd22010b80d95cd6ebfadd600564d5b3f
λx.x <lambdax-x@users.noreply.github.com>
2017-06-28 23:14:52 +0200

    Reoganize bench code

--- src/bench.rs
+++ src/bench.rs
@@ -2,110 +2,117 @@ use test::Bencher;
 use rand::random;
 use super::*;
 
-macro_rules! mk_bench {
-    ($name: ident,
-     [$($stmt: stmt),+],
-     $expr: expr
-    ) => {
-        #[bench]
-        fn $name(b: &mut Bencher) {
-            $($stmt);+;
-            b.iter(|| $expr)
-        }
-    }
-}
-
 macro_rules! algorithms {
-    ($entry: ident) => {
-        $entry!(rust_sort, v, v.sort());
-        $entry!(insertion, v, stable::insertion::sort(&mut v));
-        $entry!(merge, v, stable::merge::sort(&mut v));
+    ($entry: ident, $vec_generator: expr) => {
+        $entry!(rust_sort,
+                v,
+                $vec_generator,
+                v.sort()
+        );
+        $entry!(insertion,
+                v,
+                $vec_generator,
+                stable::insertion::sort(&mut v)
+        );
+        $entry!(merge,
+                v,
+                $vec_generator,
+                stable::merge::sort(&mut v)
+        );
         $entry!(merge_then_insertion,
                 v,
-                stable::merge::insertion::sort(7, &mut v)
+                $vec_generator,
+                stable::merge::insertion::sort(64, &mut v)
         );
-        $entry!(selection, v, unstable::selection::sort(&mut v));
-        $entry!(quick, v, unstable::quick::sort(&mut v));
+        $entry!(selection,
+                v,
+                $vec_generator,
+                unstable::selection::sort(&mut v));
+        $entry!(quick,
+                v,
+                $vec_generator,
+                unstable::quick::sort(&mut v)
+        );
         $entry!(quick_randomized,
                 v,
+                $vec_generator,
                 unstable::quick::randomized::sort(&random, &mut v)
         );
-        $entry!(heap, v, unstable::heap::sort(&mut v));
+        $entry!(heap,
+                v,
+                $vec_generator,
+                unstable::heap::sort(&mut v)
+        );
         $entry!(couting,
                 v,
-                stable::counting::sort
-                ( *v.iter().max().unwrap_or(&0) as usize
-                , &mut v
-                , |x| *x as usize
+                $vec_generator,
+                stable::counting::sort(*v.iter().max().unwrap_or(&0) as usize,
+                                       &mut v,
+                                       |x| *x as usize
                 )
         );
     }
 }
 
-macro_rules! mk_bench_vec_u32 {
-    ( $mode: ident
+macro_rules! vec_sizes {
+    ($entry: ident) => {
+        $entry!(small, 20);
+        $entry!(medium, 200);
+        $entry!(large, 2000);
+    }
+}
+
+macro_rules! vec_types {
+    ($entry: ident, $size: expr) => {
+        $entry!(constant, vec![0 ; $size]);
+        $entry!(sorted, (0 .. $size).collect());
+        $entry!(reversed, (0 .. $size).rev().collect());
+        $entry!(small_cycle, (0 .. 10).cycle().take($size).collect());
+    }
+}
+
+macro_rules! mk_vec_size_mod {
+    ($name: ident, $size: expr) => {
+        mod $name {
+            use super::*;
+            vec_types!(mk_vec_type_mod, $size);
+        }
+    }
+}
+
+macro_rules! mk_vec_type_mod {
+    ($name: ident, $vec_generator: expr) => {
+        mod $name {
+            use super::*;
+            algorithms!(mk_bench_u32, $vec_generator);
+        }
+    }
+}
+
+macro_rules! mk_bench_u32 {
+    ( $name: ident
     , $v: ident
-    , $size: expr
-    , $name: ident
+    , $vec_generator: expr
     , $sort: expr
     ) => {
         mk_bench!($name,
-                  [let mut $v: Vec<u32> = vec_u32!($mode, $v, $size)],
+                  [let mut $v: Vec<u32> = $vec_generator],
                   $sort
         );
     }
 }
 
-macro_rules! vec_u32 {
-    (sorted, $v: ident, $size: expr) => {
-        (0..).take($size).collect()
-    };
-    (reversed, $v: ident, $size: expr) => {
-        (0 .. $size).rev().collect()
-    };
-    (constant, $v: ident, $size: expr) => {
-        (0 .. 1).cycle().take($size).collect()
-    };
-}
-
-macro_rules! mk_benches_size_mod {
-    ($algorithms: ident, $bench: ident) => {
-        mod $bench {
-            use super::*;
-            $algorithms!($bench);
+macro_rules! mk_bench {
+    ($name: ident,
+     [$($stmt: stmt),*],
+     $expr: expr
+    ) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            $($stmt);*;
+            b.iter(|| $expr)
         }
     }
 }
 
-macro_rules! mk_benches_order_mod {
-    ($algorithms: ident, $mode: ident) => {
-        mod $mode {
-            use super::*;
-
-            macro_rules! small {
-                ( $name: ident
-                , $v: ident
-                , $sort: expr
-                ) => {
-                    mk_bench_vec_u32!($mode, $v, 20, $name, $sort);
-                }
-            }
-
-            macro_rules! large {
-                ( $name: ident
-                , $v: ident
-                , $sort: expr
-                ) => {
-                    mk_bench_vec_u32!($mode, $v, 5000, $name, $sort);
-                }
-            }
-
-            mk_benches_size_mod!($algorithms, small);
-            mk_benches_size_mod!($algorithms, large);
-        }
-    }
-}
-
-mk_benches_order_mod!(algorithms, sorted);
-mk_benches_order_mod!(algorithms, reversed);
-mk_benches_order_mod!(algorithms, constant);
+vec_sizes!(mk_vec_size_mod);
